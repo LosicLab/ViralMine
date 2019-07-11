c579c3d1ecd6c4d801d6100c75a9178f6db06c11
AdrianBubie <adrian@genomoncology>
2019-07-11 14:01:30 -0400

    index setting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ author = 'Adrian Bubie'
 
 # The full version, including alpha/beta/rc tags
 release = '0.0.3'
-
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
